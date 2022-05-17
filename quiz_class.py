8f41580bff058121478a1a1b0f9f9a4eb4a77ef1
Conor <conor.ganly@yahoo.com>
2022-05-17 22:29:45 +0000

    Add static method in quiz class to validate username

--- quiz_class.py
+++ quiz_class.py
@@ -10,3 +10,21 @@ class Quiz:
         self.user_name = user_name
         self.difficulty_level = difficulty_level
         self.word_count = word_count
+
+    @staticmethod
+    def validate_user_name(name):
+        """
+        This method is used to validate the user's
+        name once they enter it to to the terminal
+        returns true/false to determine if while
+        loop will continue or break"""
+        try:
+            if len(name) > 20:
+                raise ValueError(
+                    f"Your name {name} cannot be more than 20 characters!\n"
+                    f"You entered {len(name)}!\n"
+                )
+        except ValueError as e:
+            print(f"Invalid data:\n {e} Give it another go.")
+            return False
+        return True
