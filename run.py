1cf1ba5e24350fa2ada382bc08f368ea7cda6678
Conor <conor.ganly@yahoo.com>
2022-05-17 22:55:04 +0000

    Add main function to run.py to initiate programme

--- run.py
+++ run.py
@@ -5,6 +5,13 @@ builds the quiz object and runs the quiz."""
 
 from quiz_class import Quiz
 
-squiz = Quiz("G-dog", "hard", 5)
-print(squiz.user_name, squiz.difficulty_level, squiz.word_count)
-print(squiz)
+
+def main():
+    """
+    This main function is what runs the quiz from start to finish.
+    It builds a quiz object from input collected from user and
+    also validates user input via the quiz object."""
+    print("Hello there! Welcome to the Spanish quiz!\n")
+
+
+main()
