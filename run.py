c7e5ed97b9fff6b88f4f7c28b4f429d80774ccb2
Conor <conor.ganly@yahoo.com>
2022-05-17 18:25:08 +0000

    Import quiz_class.py from run.py and test if working

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+This is where the quix runs from
+It contains the main function, which
+builds the quiz object and runs the quiz."""
+
+from quiz_class import Quiz
+
+squiz = Quiz("G-dog", "hard", 5)
+print(squiz.user_name, squiz.difficulty_level, squiz.word_count)
+print(squiz)
